226bfd11bc4cf2f29df908ca0c5a6cac05f09b7c
Pieter M Leffers <pmleffers@hotmail.com>
2020-02-17 01:46:49 -0800

    Add files via upload

--- projects/requirements.py
+++ projects/requirements.py
@@ -1,1 +1,14 @@
-print('This file installs the necessary packages needed for the project.')
+import subprocess
+import sys
+
+print("This file installs the requirements for the project.")
+
+def install(package):
+    print('Installing '+package)
+    subprocess.check_call([sys.executable, "-m", "pip", "install", package])
+
+
+install_list=['graphviz','scipy','numpy==1.18.1','pandas==1.0.1','python-git','matplotlib','seaborn','plotly','cytoolz','lz4','ipywidgets','dask-labextension==1.0.3','graphviz']
+
+for i in install_list:
+    install(i)
